7a39840c478bbbbdc39f0f0c73a345a8b35e790b
Vinod <ramdinv@gmail.com>
2018-05-21 12:51:13 -0500

    added comments and removed test logs

--- liri.js
+++ liri.js
@@ -30,16 +30,12 @@ switch (command) {
     doWhatISays();
     break;
 }
-
+//IMDBMOVIE SEARCH
 function imdbMovie(input) {
   let T = input;
   request('http://www.omdbapi.com/?t=' + T + '&y&apikey=trilogy', function (error, response, body) {
-    console.log(error, "<------------------------");
-    console.log(body, "<------------------------");
-    console.log(body.Response, "<------------------------");
-    
-
-    if (error ||null||undefined) {
+    //Trying to figure out the exact text of the error so I can use it in the if statements
+    if (error || null || "undefined") {
       request('http://www.omdbapi.com/?t=mrnobody&y&apikey=trilogy', function (error, response, body) {
         let results = JSON.parse(body);
         console.log("Movie Title: " + results.Title);
@@ -74,9 +70,8 @@ function imdbMovie(input) {
     }
   });
 }
-
+//TWITTER TWEETS
 function myTweets() {
-  //The parameters for 
   var params = {
     screen_name: 'MRVMNST',
     q: "since:2018",
@@ -91,7 +86,7 @@ function myTweets() {
     }
   });
 }
-
+//SPOTIFY SEARCH
 function spotifyMusic(input, nodeArgs) {
   var userInpt = "";
   for (var i = 3; i < nodeArgs.length; i++) {
@@ -114,22 +109,19 @@ function spotifyMusic(input, nodeArgs) {
     }
   });
 }
-
+//WRONG INPUT FOR SPOTIFY DEFAULT MUSIC
 function aceOfBase() {
   spotify.search({ type: 'track', query: 'Ace of Base the Sign', limit: 1 }, function (err, data) {
-
     console.log("Artist Name: ", data.tracks.items[0].artists[0].name, );
     console.log("Song Name:", data.tracks.items[0].name);
     console.log("Preview Link:", data.tracks.items[0].href);
     console.log("Album Name: ", data.tracks.items[0].album.name, );
   });
 }
-
+// LIRI TEXT READER
 function doWhatISays() {
   fs.readFile("random.txt", "utf8", function (error, data) {
-
     let obj = JSON.parse(data);
-    console.log(obj, "is it an object");
     let textInpt = obj[1];
    spotify.search({ type: 'track', query: textInpt, limit: 1 }, function (err, data) {
     if (err) {
